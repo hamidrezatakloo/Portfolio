a441d653b7ccb6179e2fee92bd798b1ae7f71079
hamidreza <takloohamidreza@gmail.com>
2022-12-26 05:10:43 -0500

    Add Intro component to App

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
+import Intro from "./components/Intro";
+
 function App() {
-  return <div className="bg-[#061229] p-10 lg:p-20"></div>;
+  return (
+    <div className="bg-[#061229] p-10 lg:p-20">
+      <Intro />
+    </div>
+  );
 }
 
 export default App;
