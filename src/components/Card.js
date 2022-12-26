9543cc851b024280fd8c28b8d29ffe4d97c7e525
hamidreza <takloohamidreza@gmail.com>
2022-12-26 06:22:42 -0500

    Feat: Add shine effect to card component image
    Note: We used before and slid animation create in prev step

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,7 +1,9 @@
 const Card = ({ src, name }) => {
   return (
     <div className="flex flex-col items-center gap-4 ">
-      <img src={src} className="w-16 sm:w-24 lg:w-36 rounded-lg" />
+      <div className="overflow-hidden relative before:skew-x-[-35deg] before:rounded before:absolute before:w-4/12 before:h-full before:bg-zinc-100/20 before:left-[-60px] before:top-0 before:animate-slide">
+        <img src={src} className="w-16 sm:w-24 lg:w-36 rounded-lg" />
+      </div>
       <span className="capitalize font-passion text-primary text-2xl sm:text-4xl lg:text-5xl">
         {name}
       </span>
