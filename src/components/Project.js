f1d6a9c8e553f978c404aabd0d8061661010d2f9
hamidreza <takloohamidreza@gmail.com>
2022-12-28 10:35:37 -0500

    Feat: Add a component for showing project
    Note: Each project has fallowing props:
    img: screenShot of project
    title: title
    desc: description
    tags: a list of tech tags that used in project
    link: a link to live demo of project
    repo: url of project repository

---
+++ src/components/Project.js
@@ -0,0 +1,36 @@
+import Tag from "./Tag";
+
+const Project = ({ img, title, desc, tags, link, repo }) => {
+  return (
+    <div className="flex flex-col bg-light text-white font-calibre rounded my-12">
+      <div className="flex flex-col lg:flex-row">
+        <img src={img} className="lg:w-6/12" />
+        <div className="p-2 flex-1">
+          <h2 className="my-8 font-semibold text-4xl 2xl:mt-28">{title}</h2>
+          <p className="lg:text-xl xl:text-2xl">{desc}</p>
+        </div>
+      </div>
+      <div className="flex flex-col  lg:flex-row items-center p-2">
+        <div className="break-words ml-2 my-10 flex-1">
+          {tags.map((tag, index) => (
+            <Tag key={index} name={tag.name} color={tag.color} />
+          ))}
+        </div>
+        <div className="md:w-6/12 lg:w-4/12 text-center flex items-center justify-center gap-4">
+          <a href={repo}>
+            <img src="github.png" className="w-12 hover:scale-105" />
+          </a>
+          <a
+            href={link}
+            className="group h-[40px] rounded z-10 font-semibold text-white text-lg uppercase bg-blue-800 px-12 py-2 my-4 lg:my-0 inline-block relative hover:text-blue-800"
+          >
+            <div className="absolute w-0 h-full -z-10 rounded group-hover:w-full bg-white left-0 top-0 transition-all"></div>
+            Open Live
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Project;
