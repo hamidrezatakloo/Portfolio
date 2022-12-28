e43402d865c8ef3a77943f5612be0970c8300683
hamidreza <takloohamidreza@gmail.com>
2022-12-28 09:57:42 -0500

    Fix: Change width of image in card component

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,7 +2,7 @@ const Card = ({ src, name }) => {
   return (
     <div className="flex flex-col items-center gap-4 ">
       <div className="overflow-hidden relative before:skew-x-[-35deg] before:rounded before:absolute before:w-4/12 before:h-full before:bg-zinc-100/20 before:left-[-60px] before:top-0 before:animate-slide">
-        <img src={src} className="w-16 sm:w-24 lg:w-36 rounded-lg" />
+        <img src={src} className="w-16 sm:w-24 lg:w-30" />
       </div>
       <span className="capitalize font-passion text-primary text-2xl sm:text-4xl lg:text-5xl">
         {name}
