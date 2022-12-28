1f34f54bcc2676fe6e3148108b1b1c2f3f523586
hamidreza <takloohamidreza@gmail.com>
2022-12-28 10:04:11 -0500

    Fix: Add Skill component to App

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import Intro from "./components/Intro";
-
+import Skills from "./components/Skills";
 function App() {
   return (
     <div className="bg-[#061229] p-10 lg:p-20">
       <Intro />
+      <Skills />
     </div>
   );
 }
