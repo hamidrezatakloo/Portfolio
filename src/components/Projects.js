bafac73cade30e86156c374dd4f6a5b34ee45dfd
hamidreza <takloohamidreza@gmail.com>
2022-12-28 10:42:13 -0500

    Feat: Add a section for showing all projects
    Note: This component has a header section and render project lists
    with Project component

---
+++ src/components/Projects.js
@@ -0,0 +1,23 @@
+import HeaderSec from "./HeaderSec";
+import ProjectList from "../projectList";
+import Project from "./Project";
+const Projects = () => {
+  return (
+    <section className="mt-[100px]">
+      <HeaderSec text="The projects that i build" />
+      {ProjectList.map((project, index) => (
+        <Project
+          key={index}
+          img={project.img}
+          title={project.title}
+          desc={project.desc}
+          tags={project.tags}
+          link={project.link}
+          repo={project.repo}
+        />
+      ))}
+    </section>
+  );
+};
+
+export default Projects;
