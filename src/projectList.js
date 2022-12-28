390d22c2a98fa76ec0195bcee938c0deb0bc3da4
hamidreza <takloohamidreza@gmail.com>
2022-12-28 10:41:11 -0500

    Feat: Create a project list as js object

---
+++ src/projectList.js
@@ -0,0 +1,113 @@
+const ProjectList = [
+  {
+    img: "super_blog.png",
+    title: "Super Blog",
+    desc: "super Blog is a webapp that every one can join and write their post or read others post",
+    tags: [
+      {
+        name: "React",
+        color: "#06b6d4",
+      },
+      {
+        name: "tailwindcss",
+        color: "#06b6d4",
+      },
+      {
+        name: "redux",
+        color: "#5b21b6",
+      },
+      {
+        name: "django",
+        color: "#15803d",
+      },
+      {
+        name: "djoser",
+        color: "#15803d",
+      },
+      {
+        name: "djangoRestFramework",
+        color: "#15803d",
+      },
+    ],
+    link: "https://hamidrezatakloo.github.io/super_blog/",
+    repo: "https://github.com/hamidrezatakloo/super_blog",
+  },
+  {
+    img: "shopping-cart.png",
+    title: "ShoppingCart",
+    desc: "shoppingCart is a store of actionFigures",
+    tags: [
+      {
+        name: "React",
+        color: "#06b6d4",
+      },
+      {
+        name: "tailwindcss",
+        color: "#06b6d4",
+      },
+    ],
+    link: "https://hamidrezatakloo.github.io/ShoppingCart/",
+    repo: "https://github.com/hamidrezatakloo/ShoppingCart",
+  },
+  {
+    img: "cv-creator.png",
+    title: "Cv Creator",
+    desc: "A tool to create resume",
+    tags: [
+      {
+        name: "React",
+        color: "#06b6d4",
+      },
+      {
+        name: "tailwindcss",
+        color: "#06b6d4",
+      },
+    ],
+    link: "https://hamidrezatakloo.github.io/cv-creator/",
+    repo: "https://github.com/hamidrezatakloo/cv-creator",
+  },
+  {
+    img: "crypto-app.png",
+    title: "Crypto App",
+    desc: "Showing open and close price of BTC,ETH,USDT",
+    tags: [
+      {
+        name: "Html",
+        color: "#f97316",
+      },
+      {
+        name: "Sass",
+        color: "#db2777",
+      },
+      {
+        name: "Javascript",
+        color: "#facc15",
+      },
+      {
+        name: "Webpack",
+        color: "#2563eb",
+      },
+    ],
+    link: "https://hamidrezatakloo.github.io/cryptoApp/",
+    repo: "https://github.com/hamidrezatakloo/cryptoApp",
+  },
+  {
+    img: "knightMove-bfs.png",
+    title: "knight Move",
+    desc: "find shortest path to goal in 8*8 chess board with bfs",
+    tags: [
+      {
+        name: "Javascript",
+        color: "#facc15",
+      },
+      {
+        name: "jest",
+        color: "#9d174d",
+      },
+    ],
+    link: "https://github.com/hamidrezatakloo/knightMove-bfs",
+    repo: "https://github.com/hamidrezatakloo/knightMove-bfs",
+  },
+];
+
+export default ProjectList;
