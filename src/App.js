8f200de751f74de5977d153004e62638f65fe375
hamidreza <takloohamidreza@gmail.com>
2022-12-28 10:44:03 -0500

    Fix: Add Projects  component to App

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Intro from "./components/Intro";
+import Projects from "./components/Projects";
 import Skills from "./components/Skills";
 function App() {
   return (
     <div className="bg-[#061229] p-10 lg:p-20">
       <Intro />
       <Skills />
+      <Projects />
     </div>
   );
 }
